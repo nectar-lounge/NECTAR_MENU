9be58099cf6ae9f20a8c3dd4d3e432b5755d2d27
hormigon-143 <Hormigon@mail.ru>
2026-08-20 17:08:07 +0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,749 @@
+const MENU = [
+  {
+    "id": "1-1",
+    "category": "Холодные закуски",
+    "name": "Мясное плато (казы, жая, жал)",
+    "price": 4990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "1-2",
+    "category": "Холодные закуски",
+    "name": "Домашние соленья",
+    "price": 2890,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "1-3",
+    "category": "Холодные закуски",
+    "name": "Овощная нарезка",
+    "price": 2890,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "1-4",
+    "category": "Холодные закуски",
+    "name": "Рыбное ассорти",
+    "price": 5590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "1-5",
+    "category": "Холодные закуски",
+    "name": "Капрезе",
+    "price": 2790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "1-6",
+    "category": "Холодные закуски",
+    "name": "Сырное плато",
+    "price": 4990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-1",
+    "category": "Салаты",
+    "name": "Свежий овощной салат",
+    "price": 1990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-2",
+    "category": "Салаты",
+    "name": "Аччик-чучук",
+    "price": 1790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-3",
+    "category": "Салаты",
+    "name": "Цезарь с курицей",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-4",
+    "category": "Салаты",
+    "name": "Хрустящие баклажаны",
+    "price": 2790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-5",
+    "category": "Салаты",
+    "name": "Греческий салат",
+    "price": 2490,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-6",
+    "category": "Салаты",
+    "name": "Теплый салат с кониной",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "2-7",
+    "category": "Салаты",
+    "name": "Салат с креветками и рукколой",
+    "price": 3990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "3-1",
+    "category": "Горячие закуски",
+    "name": "Креветки темпура",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "3-2",
+    "category": "Горячие закуски",
+    "name": "Клаб-сэндвич с курицей (подается с картофелем фри)",
+    "price": 3190,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "3-3",
+    "category": "Горячие закуски",
+    "name": "Кесадилья с курицей",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "3-4",
+    "category": "Горячие закуски",
+    "name": "Чебуреки",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "3-5",
+    "category": "Горячие закуски",
+    "name": "Наггетсы куриные",
+    "price": 1690,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-1",
+    "category": "Снэки",
+    "name": "Большой сет закусок (креветки, гренки, чечил, фри, сосиски, луковые кольца)",
+    "price": 6990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-2",
+    "category": "Снэки",
+    "name": "Креветки пивные",
+    "price": 3990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-3",
+    "category": "Снэки",
+    "name": "Фисташки",
+    "price": 1590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-4",
+    "category": "Снэки",
+    "name": "Чесночные гренки",
+    "price": 1590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-5",
+    "category": "Снэки",
+    "name": "Чечил копченый",
+    "price": 1390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "4-6",
+    "category": "Снэки",
+    "name": "Арахис",
+    "price": 1390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-1",
+    "category": "Супы",
+    "name": "Окрошка с мясом (сезонная)",
+    "price": 2390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-2",
+    "category": "Супы",
+    "name": "Куриный суп с лапшой",
+    "price": 1590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-3",
+    "category": "Супы",
+    "name": "Солянка мясная",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-4",
+    "category": "Супы",
+    "name": "Том Ям",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-5",
+    "category": "Супы",
+    "name": "Рамен с говядиной",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-6",
+    "category": "Супы",
+    "name": "Рамен с курицей",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-7",
+    "category": "Супы",
+    "name": "Чечевичный крем-суп",
+    "price": 1990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-8",
+    "category": "Супы",
+    "name": "Шорпа",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "5-9",
+    "category": "Супы",
+    "name": "Пельмени (с бульоном / без)",
+    "price": 1990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-1",
+    "category": "Пицца и выпечка",
+    "name": "Пицца Пепперони",
+    "price": 3090,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-2",
+    "category": "Пицца и выпечка",
+    "name": "Пицца Маргарита",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-3",
+    "category": "Пицца и выпечка",
+    "name": "Пицца с курицей и грибами",
+    "price": 3090,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-4",
+    "category": "Пицца и выпечка",
+    "name": "Пицца Болоньезе",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-5",
+    "category": "Пицца и выпечка",
+    "name": "Мексиканская пицца с халапеньо",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-6",
+    "category": "Пицца и выпечка",
+    "name": "Хачапури по-мегрельски",
+    "price": 2790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-7",
+    "category": "Пицца и выпечка",
+    "name": "Хлебная корзина",
+    "price": 890,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "6-8",
+    "category": "Пицца и выпечка",
+    "name": "Лепешка",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "7-1",
+    "category": "Паста",
+    "name": "Фарфалле с морепродуктами (креветки, мидии, кальмары)",
+    "price": 4990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "7-2",
+    "category": "Паста",
+    "name": "Карбонара",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "7-3",
+    "category": "Паста",
+    "name": "Болоньезе",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "7-4",
+    "category": "Паста",
+    "name": "Фетучини с курицей и грибами",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-1",
+    "category": "Горячие блюда",
+    "name": "Куырдак",
+    "price": 4090,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-2",
+    "category": "Горячие блюда",
+    "name": "Плов",
+    "price": 3090,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-3",
+    "category": "Горячие блюда",
+    "name": "Бургер с курицей (подается с фри и соусом)",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-4",
+    "category": "Горячие блюда",
+    "name": "Бургер с говядиной (подается с фри и соусом)",
+    "price": 3590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-5",
+    "category": "Горячие блюда",
+    "name": "Бургер, грибной стаут",
+    "price": 4390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-6",
+    "category": "Горячие блюда",
+    "name": "Колбаски говяжьи / бараньи",
+    "price": 3390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-7",
+    "category": "Горячие блюда",
+    "name": "Колбаски куриные",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-8",
+    "category": "Горячие блюда",
+    "name": "Говядина по-тайски с рисом",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-9",
+    "category": "Горячие блюда",
+    "name": "Бефстроганов с пюре",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-10",
+    "category": "Горячие блюда",
+    "name": "Куриная грудка в сырно-сливочном соусе",
+    "price": 3290,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "8-11",
+    "category": "Горячие блюда",
+    "name": "Картофель по-домашнему с мясом",
+    "price": 3990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-1",
+    "category": "Шашлык на мангале",
+    "name": "Антрекот",
+    "price": 3990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-2",
+    "category": "Шашлык на мангале",
+    "name": "Баранина",
+    "price": 3190,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-3",
+    "category": "Шашлык на мангале",
+    "name": "Люля-кебаб",
+    "price": 3190,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-4",
+    "category": "Шашлык на мангале",
+    "name": "Куриное филе",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-5",
+    "category": "Шашлык на мангале",
+    "name": "Куриные крылышки",
+    "price": 2890,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-6",
+    "category": "Шашлык на мангале",
+    "name": "Утка",
+    "price": 2790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-7",
+    "category": "Шашлык на мангале",
+    "name": "Куриные окорочка",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-8",
+    "category": "Шашлык на мангале",
+    "name": "Овощи гриль на шпажке",
+    "price": 1990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "9-9",
+    "category": "Шашлык на мангале",
+    "name": "Шампиньоны на мангале",
+    "price": 1790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "10-1",
+    "category": "Гарниры",
+    "name": "Овощи гриль",
+    "price": 1990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "10-2",
+    "category": "Гарниры",
+    "name": "Брокколи",
+    "price": 1790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "10-3",
+    "category": "Гарниры",
+    "name": "Картофельные дольки",
+    "price": 1390,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "10-4",
+    "category": "Гарниры",
+    "name": "Картофель фри",
+    "price": 1190,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "10-5",
+    "category": "Гарниры",
+    "name": "Рис",
+    "price": 790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "11-1",
+    "category": "Соусы",
+    "name": "Сырный",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "11-2",
+    "category": "Соусы",
+    "name": "Красный",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "11-3",
+    "category": "Соусы",
+    "name": "Белый",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "11-4",
+    "category": "Соусы",
+    "name": "Кетчуп",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "11-5",
+    "category": "Соусы",
+    "name": "Тартар",
+    "price": 590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "12-1",
+    "category": "Десерты",
+    "name": "Восточные сладости",
+    "price": 5490,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "12-2",
+    "category": "Десерты",
+    "name": "Пирожное в ассортименте",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "12-3",
+    "category": "Десерты",
+    "name": "Арбуз, дыня (по сезону)",
+    "price": 1490,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "12-4",
+    "category": "Десерты",
+    "name": "Фруктовая ваза",
+    "price": 7500,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "13-1",
+    "category": "Безалкогольные напитки",
+    "name": "Фирменные лимонады (Вишня – Виноград, Киви-Грейпфрут-Жасмин, Маракуйя-Ананас, Нектар)",
+    "price": 2990,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "13-2",
+    "category": "Безалкогольные напитки",
+    "name": "Авторский чай (Алматинский, Ташкентский, Малина-Лайм, Персик-жасмин)",
+    "price": 2590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "13-3",
+    "category": "Безалкогольные напитки",
+    "name": "Пепси",
+    "price": 1790,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  },
+  {
+    "id": "13-4",
+    "category": "Безалкогольные напитки",
+    "name": "Листовой чай (Ассам, Сенча, Жасмин, Эрл Грей, Ройбуш, Султан)",
+    "price": 1590,
+    "weight": "",
+    "composition": "",
+    "note": ""
+  }
+];
